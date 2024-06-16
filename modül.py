e9f965d94e5cc4329eaada2962fe67e5d40607c0
enesxsec <120325383+ghost0x02@users.noreply.github.com>
2024-06-16 09:53:35 +0300

    Create modül.py

---
+++ modül.py
@@ -0,0 +1,11 @@
+import subprocess
+import platform
+import socket
+import dns.resolver
+import whois
+import requests
+import os
+import time
+import sys
+from bs4 import BeautifulSoup
+from colorama import Fore, Style
